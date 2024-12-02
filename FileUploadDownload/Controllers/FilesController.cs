08f1d06a60c2407efcae5e030de4d773a0e02df3
srikanthcbs123 <165771133+srikanthcbs123@users.noreply.github.com>
2024-12-02 07:47:58 +0530

    Add project files.

---
+++ FileUploadDownload/Controllers/FilesController.cs
@@ -0,0 +1,80 @@
+﻿//using FileUploadDownload.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace FileUploadDownload.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+
+        [HttpPost]
+        [Route("UploadFile")]
+        public async Task<IActionResult> UploadFile([FromForm] IFormFile file,[FromForm] string ImageCaption, CancellationToken cancellationtoken)
+        {
+            var obj = ImageCaption;
+            var result = await WriteFile(file);
+            return Ok(new { response = result });
+        }
+
+        private async Task<string> WriteFile(IFormFile file)
+        {
+
+            //string firstname = "srikanth";
+            //string lastname = "hhhhh";
+            //string fullname = firstname + lastname;
+
+            //string path1 = @"D:\Chsharp";
+            //string path2 = @"E:\Movies";
+            //var fullpath = Path.Combine(path1, path2);
+
+
+
+            var info = Directory.GetCurrentDirectory();
+            string filename = "";
+            try
+            {
+                var extension = "." + file.FileName.Split('.')[file.FileName.Split('.').Length - 1];
+                filename = DateTime.Now.Ticks.ToString() + extension;
+
+                var filepath = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\Files");
+
+                if (!Directory.Exists(filepath))
+                {
+                    Directory.CreateDirectory(filepath);
+                }
+
+                var exactpath = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\Files", filename);
+                using (var stream = new FileStream(exactpath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return filename;
+        }
+
+
+
+        [HttpGet]
+        [Route("DownloadFile")]
+        public async Task<IActionResult> DownloadFile(string filename)
+        {
+            var filepath = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\Files",filename);
+
+            var provider = new FileExtensionContentTypeProvider();
+            if (!provider.TryGetContentType(filepath, out var contenttype))
+            {
+                contenttype = "application/octet-stream";
+            }
+
+            var bytes = await System.IO.File.ReadAllBytesAsync(filepath);
+            return File(bytes, contenttype, Path.GetFileName(filepath));
+        }
+
+    }
+}
